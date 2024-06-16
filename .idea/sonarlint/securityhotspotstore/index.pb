98fedd631f97152dcf8d9da52352f1e2ebe726b8
prageeth-thilakarathna <ygprageethchamara2002@gmail.com>
2024-06-16 12:31:24 +0530

    dashboard user interface design update and user management interface design

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -1,6 +1,4 @@
 
-T
-$src/main/java/edu/icet/pos/Main.java,d\c\dc1c741ceee4faac312666a4d4e71dde067aba98
 7
 pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
 9
@@ -9,3 +7,11 @@ t
 Dsrc/main/java/edu/icet/pos/controller/DashboardLayoutController.java,2\8\285abcc9f05d952fd7e38ee6d854981b7791e65b
 G
 src/main/java/Main.java,e\4\e44a8606ce010ad9b89aafdfcee2ab49afbf6d10
+i
+9src/main/java/edu/icet/pos/controller/UserController.java,4\a\4a34a5a55f993d0d1ec07a68ebb95e1e711b40a3
+n
+>src/main/java/edu/icet/pos/controller/user/FormController.java,3\f\3f9877317b790050c0da3efebe801d8116560d5d
+p
+@src/main/java/edu/icet/pos/controller/user/SearchController.java,2\4\24870745538c45cebeec881a4989f5b5362f39d2
+n
+>src/main/java/edu/icet/pos/controller/user/ViewController.java,6\a\6a4e28ffde02e4611e56ed3df79486d8d0659a5a
--- src/main/java/edu/icet/pos/controller/DashboardLayoutController.java
+++ src/main/java/edu/icet/pos/controller/DashboardLayoutController.java
@@ -1,4 +1,55 @@
 package edu.icet.pos.controller;
 
-public class DashboardLayoutController {
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DashboardLayoutController implements Initializable {
+    @FXML
+    private VBox bottomVBox;
+    @FXML
+    private VBox rightVBox;
+    @FXML
+    private VBox centerVBox;
+    @FXML
+    private Label header;
+    @FXML
+    private Button btnUser;
+
+    @FXML
+    private void btnUserAction() throws IOException {
+
+        /*btnUser.setStyle("-fx-background-color: #0c7675; -fx-background-radius: 10px;");
+        centerVBox.getChildren().clear();
+        centerVBox.getChildren().add(parent);*/
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        header.setText("User");
+        try{
+            Parent parentForm = new FXMLLoader(getClass().getResource("/view/user/form.fxml")).load();
+            centerVBox.getChildren().clear();
+            centerVBox.getChildren().add(parentForm);
+            btnUser.setStyle("-fx-background-color: #0c7675; -fx-background-radius: 10px;");
+
+            Parent parentSearch = new FXMLLoader(getClass().getResource("/view/user/search.fxml")).load();
+            rightVBox.getChildren().clear();
+            rightVBox.getChildren().add(parentSearch);
+
+            Parent parentView = new FXMLLoader(getClass().getResource("/view/user/view.fxml")).load();
+            bottomVBox.getChildren().clear();
+            bottomVBox.getChildren().add(parentView);
+        } catch (IOException e) {
+
+        }
+    }
 }
